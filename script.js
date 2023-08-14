062a02719b3bb3f969db8319c6473c9f56941ced
dielawn <dielawnmerkill@gmail.com>
2023-08-14 05:15:09 -0600

    setShipLocation function and test

--- script.js
+++ script.js
@@ -1,47 +1,116 @@
 // const containerDiv = document.getElementById('container')
+class Game {
+    constructor() {
+        this.player1 = new Player('Sid')
+        this.player2 = new Player('Nancy')
 
-//four grids two for each player 10 x 10. a-j 1-10
+    }
+}
+
+
 class Grid {
     constructor() {
         this.yAxis = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
         this.xAxis = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
-        this.board = this.createBoard()
+        this.grid = this.createGrid()
     }
-    createBoard() {
+    createGrid() {
         const grid = []
+
         for (let i = 0; i < this.yAxis.length; i++) {
             for (let j = 0; j < this.xAxis.length; j++) {
-                grid.push([this.xAxis[j], this.yAxis[i]])
+                grid.push([this.xAxis[j] + this.yAxis[i]])
             }
         }
         return grid
     }
     findIndex(x, y) {
-        const board = this.board
-        for (let i = 0; i < board.length; i++) {
-            if (board[i][0] === x && board[i][1] === y) {
-             return board[i]
+        const grid = this.grid
+        for (let i = 0; i < grid.length; i++) {
+            if (grid[i][0] === x && grid[i][1] === y) {
+             return grid[i]
             }
         }
         return 'Target not found'
     }
     isValid(x, y) {
-        return this.board.some(element => element[0] === x && element[1] === y)
+        return this.grid.some(element => element[0] === x && element[1] === y)
     }
+   
+    //recievAttack() {}
 } 
 
-const testGrid = new Grid()
+class Ship {
+    constructor(shipType) {
+        this.ship = shipType
+        this.hitPoints = this.ship.length
+        this.isSunk = false
+    }
+    hit() {
+      this.hitPoints - 1
+      if (this.hitPoints <= 0) {
+        this.isSunk = true
+        return `You sunk my ${this.ship.name}!`
+      }
+      
+    }    
 
-testGrid.findIndex('a', '1')
-//carrier length 5
-//battlship 4
-//destroyer 3
-//submarine 3
-//patrol boat 2
 
+}
 
+class Player {
+    constructor(name, ) {
+        this.name = name
+        this.revealedBoard = new Grid()        
+        this.hiddenBoard = new Grid()
+        this.occupiedCoordinates = []
+        this.choosenCoordinates = []        
+        this.ships = this.createShips()
+        this.isTurn = false
+        
+    }
+    createShips() {
+        const ships = [
+            { name: 'Carrier', length: 5, shipLocation: [0,0,0,0,0] },//ship 0
+            { name: 'Battleship', length: 4, shipLocation: [0,0,0,0] },  //ship 1
+            { name: 'Destroyer', length: 3, shipLocation: [0,0,0] },  //ship 2
+            { name: 'Submarine', length: 3, shipLocation: [0,0,0] },  //ship 3
+            { name: 'Patrol', length: 2, shipLocation: [0,0]}  //ship 4
+        ]
+        const playerShips = []
+        for(const ship of ships) {
+            playerShips.push(new Ship(ship))
+        }
+        return playerShips
+    }
+    setShipLocation(shipsIndex, coordinatesArray) {
+       
+       const ship = this.ships[shipsIndex].ship
+       const shipsCoords = []
+       for (let i = 0; i < ship.length; i++) {
+        ship.shipLocation[i] = coordinatesArray[i]
+        shipsCoords.push(ship.shipLocation[i])
+       }
+       this.occupiedCoordinates.push(shipsCoords)
+       return shipsCoords
+    }
+    fire(coords) {
+        const selected = []
+        selected.push(coords)
+        this.choosenCoordinates.push(selected)
+    }
 
+}
 
+const newGame = new Game()
+const carrierCoord = ['a1', 'a2', 'a3', 'a4', 'a5']
+const battleshipCoord = ['b2', 'b3', 'b4', 'b5']
+
+newGame.player2.setShipLocation(0, carrierCoord)
+newGame.player2.setShipLocation(1, battleshipCoord)
 module.exports = {
     Grid,
+    Player,
+    Ship,
+    Game,
 }
--- script.test.js
+++ script.test.js
@@ -1,20 +1,38 @@
 
-const { Grid,  } = require('./script')
+const { Grid, Player, Ship, Game,} = require('./script')
 
 describe('10 x 10 grid, where every square has coordinates and a grid index', () => {
     const testGrid = new Grid()
     test('checks for a grid length of 100', () => {
-        testGrid.createBoard()
-        expect(testGrid.board.length).toBe(100)
+        testGrid.createGrid()
+        expect(testGrid.grid.length).toBe(100)
     })
     
     test('returns a grid index from x y coordinates', () => {
-        expect(testGrid.findIndex('a', '1')).toBe(testGrid.board[0])
-        expect(testGrid.findIndex('j', '10')).toBe(testGrid.board[99])
+        expect(testGrid.findIndex('a1')).toBe(testGrid.grid[0])
+        expect(testGrid.findIndex('j10')).toBe(testGrid.grid[99])
     })
     test('check if board coordinates are truthy', () => {
-        expect(testGrid.isValid('a', '1')).toBeTruthy()
-        expect(testGrid.isValid('a', '11')).toBeFalsy()
+        expect(testGrid.isValid('a1')).toBeTruthy()
+        expect(testGrid.isValid('a11')).toBeFalsy()
     })
 
 })
+
+describe('Player class has a name, 2 grids, and a fleet of ships', () => {
+    const testPlayer = new Player('testPlayer')
+    test('test player name', () => {
+        expect(testPlayer.name).toBe('testPlayer')
+    })
+    test('player revealed board occupied coordinates', () => {
+        expect(testPlayer.setShipLocation(0, ['a1', 'a2', 'a3', 'a4', 'a5'])).toBe(testPlayer.occupiedCoordinates[0])
+    })
+    // test('player hidden board choosen coordinates')
+}) 
+
+// describe('Ship class keeps track of hits, sets isSunk', () => {
+//     const testShips =new Ship()
+//     test('decrease hit count', () => {
+//         expect()
+//     })
+// })
